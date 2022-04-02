1cec17bca7bc6ed1a1bfb4167e5301d840f4e5ea
a.hussain.kitopi <amir@kitopi.com>
2022-04-03 01:22:00 +0400

    Merging Dev and Master

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p >
+          <strong>
+            Some people? Are there some people?
+          </strong>
         </p>
         <a
           className="App-link"
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React some more
         </a>
       </header>
     </div>
